ae1148b1cc5a50dd2ac3bd34b96d02d89db1ab08
Wibinah <wibinahtetuh@gmail.com>
2023-05-16 11:24:27 +0100

    first phase portfolio

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './header.css'
+import './CTA'
+import CTA from './CTA' //here we import the CTA component inside our header
+import me from '../../assets/mepic.png'
+import HeaderSocial from './HeaderSocial' //import of headerSocial component
+export const Header = () => {
+ return (
+  <header>
+    <div className='container header__container'>
+     <h5>Hello I'm</h5>
+     <h1>TETUH WIBINAH ENGONWEI</h1>
+    <h5 className='text-light'>Computer Engineer </h5>
+  <CTA/>
+  <HeaderSocial/>
+<div className='my_image'>
+    <img src={me} alt='mepic'/>
+</div>
+<a href='#contact' className='scroll__down'>FET-L300</a>
+</div>
+</header>
+)
+}
+export default Header;
